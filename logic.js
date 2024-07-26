fc08777c304b6a585ea5adf1572e212a782418e7
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-07-26 13:18:01 +0300

    add all

---
+++ logic.js
@@ -0,0 +1,36 @@
+// let a = true;
+// let b = false;
+// let c = false;
+
+// console.log(a&c);
+// console.log(a&b);
+// console.log(b&c);
+
+// console.log(a||b);
+// console.log(a||c);
+// console.log(b||c);
+
+
+// let a = true;
+// let b = false;
+
+// console.log(!a);
+// console.log(!b);
+
+// let age = 25;
+// let hasLicense = true;
+
+// if(age > 18 && hasLicense) {
+//     console.log("You can drive.");
+// } else{
+//     console.log("You can't drive.");
+// }
+
+let age = 25;
+let hasLicense = true;
+
+if(age < 18 ||!hasLicense) {
+    console.log("You can't drive.");
+} else {
+    console.log("You can drive.");
+}
